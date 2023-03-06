bec2b2e86ab9c7212924c5f7878c387295dce05c
Stefan Tatschner <stefan@rumpelsepp.org>
2023-03-06 14:15:33 +0100

    fix clippy lint

--- src/base64.rs
+++ src/base64.rs
@@ -3,10 +3,9 @@ use serde::Serialize;
 use serde::Serializer;
 
 pub fn serialize<S: Serializer>(v: &Option<Vec<u8>>, s: S) -> Result<S::Ok, S::Error> {
-    let base64 = match v {
-        Some(v) => Some(base64::engine::general_purpose::STANDARD.encode(v)),
-        None => None,
-    };
+    let base64 = v
+        .as_ref()
+        .map(|v| base64::engine::general_purpose::STANDARD.encode(v));
     <Option<String>>::serialize(&base64, s)
 }
 
